ba03cf2d3544e871e5616a8aa810ea75e6ad0ba7
LORTEY <wnozka19@gmail.com>
2025-01-04 16:22:46 +0000

    Added Flashcard directory and now flashcard sets are read from it

--- app/src/main/java/com/example/cardflare/ui/theme/MainMenuRender.kt
+++ app/src/main/java/com/example/cardflare/ui/theme/MainMenuRender.kt
@@ -1,22 +1,13 @@
 package com.example.cardflare.ui.theme
-
-import android.R.attr.padding
-import android.R.attr.start
-import android.content.res.AssetManager
 import android.util.Log
-import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -34,7 +25,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -42,205 +32,194 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.rememberAsyncImagePainter
-import coil.decode.SvgDecoder
-import coil.request.ImageRequest
 import com.example.cardflare.R
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
-import android.Manifest
 import android.content.Context
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.TabRowDefaults.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.*
-import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 
 
-private var currentMenu = 1;
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainMenuRender(context: Context) {
-    if (currentMenu == 1){
-        var searchQuery by remember { mutableStateOf("") }
-        var appear by remember { mutableStateOf(false) }
-        val screenHeight = LocalConfiguration.current.screenHeightDp
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .background(Color(ColorPalette.sa10))) {
+    var searchQuery by remember { mutableStateOf("") }
+    var appear by remember { mutableStateOf(false) }
+    val screenHeight = LocalConfiguration.current.screenHeightDp
+    var files = listFilesInAssets(context).toList()
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(ColorPalette.sa10))) {
 
-            Column(modifier = Modifier.fillMaxSize()){
-                Spacer(modifier = Modifier.height((screenHeight * 0.12f).dp))
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1.0f)) {
-                    LazyColumn(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        contentPadding = PaddingValues(16.dp),
-                        verticalArrangement = Arrangement.spacedBy(8.dp)
-                    )
-                    {
-                        items(20) {
-                            Row(
-                                horizontalArrangement = Arrangement.spacedBy(10.dp),
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(100.dp)
-                            ) {
+        Column(modifier = Modifier.fillMaxSize()){
+            Spacer(modifier = Modifier.height((screenHeight * 0.12f).dp))
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .weight(1.0f)) {
+                LazyVerticalGrid (
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentPadding = PaddingValues(16.dp),
+                    columns = GridCells.Fixed(2),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                )
+                {
+                    items(files) { item ->
+
+                            if (searchQuery in item.toString() || searchQuery=="") {
                                 Text(
-                                    text = "flashcardset",
+                                    text = item.toString(),
                                     color = Color(ColorPalette.pa0),
                                     modifier = Modifier
                                         .background(
                                             Color(ColorPalette.sa20),
                                             shape = RoundedCornerShape(10.dp)
                                         )
-                                        .fillMaxWidth(0.1f)
-                                        .height(100.dp)
-                                        .weight(1f)
-                                        .padding(10.dp)
-                                        .clickable { }
-                                )
-                                Text(
-                                    text = "flashcardset",
-                                    color = Color(ColorPalette.pa0),
-                                    modifier = Modifier
-                                        .background(
-                                            Color(ColorPalette.sa20),
-                                            shape = RoundedCornerShape(10.dp)
-                                        )
-                                        .fillMaxWidth(0.1f)
-                                        .weight(1f)
+                                        .fillMaxWidth(0.5f)
                                         .height(100.dp)
                                         .padding(10.dp)
                                         .clickable { }
                                 )
-                            }
+
                         }
                     }
-                    // Add icon menu
-                    Icon(
-                        painter = painterResource(id = R.drawable.plus_circle),
-                        contentDescription = "chart",
-                        tint = Color(ColorPalette.pa40),
-                        modifier = Modifier
-                            .size(128.dp)
-                            .padding(15.dp)
-                            .clickable { }
-                            .background(Color(ColorPalette.sa10), shape = CircleShape)
-                            .align(Alignment.BottomEnd)
-                    )
                 }
-            }
-
-
-            //fade effect and upper menu
-            Column(modifier = Modifier.height((screenHeight * 0.15f).dp)) {
-
-                Spacer(modifier = Modifier.fillMaxSize().weight(0.6f))
-                Row(horizontalArrangement = Arrangement.spacedBy(10.dp),
-                    verticalAlignment = Alignment.CenterVertically,
+                // Add menu
+                Icon(
+                    painter = painterResource(id = R.drawable.plus_circle),
+                    contentDescription = "chart",
+                    tint = Color(ColorPalette.pa40),
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 10.dp)
-                        .weight(0.6f)
-                        .weight(20f)
+                        .size(128.dp)
+                        .padding(15.dp)
+                        .background(Color(ColorPalette.sa10), shape = CircleShape)
+                        .clickable { }
+                        .align(Alignment.BottomEnd)
+                )
+            }
+        }
+
+
+        //fade effect and upper menu
+        Column(modifier = Modifier.height((screenHeight * 0.15f).dp)) {
+
+            Spacer(modifier = Modifier.fillMaxSize().weight(0.6f))
+            Row(horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+                    .weight(0.6f)
+                    .weight(20f)
+                    .fillMaxHeight()
+                    .background(Color(ColorPalette.sa20), shape = RoundedCornerShape(10.dp))
+                    .padding(horizontal = 10.dp, vertical = 10.dp)){
+                Icon(
+                    painter = painterResource(id = R.drawable.menu),
+                    contentDescription = "chart",
+                    tint = Color(ColorPalette.pa40),
+                    modifier = Modifier
                         .fillMaxHeight()
-                        .background(Color(ColorPalette.sa20), shape = RoundedCornerShape(10.dp))
-                        .padding(horizontal = 10.dp, vertical = 10.dp)){
-                    Icon(
-                        painter = painterResource(id = R.drawable.menu),
-                        contentDescription = "chart",
-                        tint = Color(ColorPalette.pa40),
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .clickable { appear = !appear }
-                            .weight(0.1f)
-                    )
+                        .clickable { appear = !appear }
+                        .weight(0.1f)
+                )
+                //I have no idea how to use the colors in TextField so to make a place holder i used this box
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+                    .weight(1.0f)
+                    .background(Color(ColorPalette.sa20), shape = RoundedCornerShape(10.dp))
+                    .align(Alignment.CenterVertically)){
                     BasicTextField(
                         value = searchQuery,
                         onValueChange = {searchQuery = it},
-                        textStyle = TextStyle(color = Color(ColorPalette.pa0), fontSize = 16.sp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 10.dp)
-                            .weight(1.0f)
-                            .background(Color(ColorPalette.sa20), shape = RoundedCornerShape(10.dp))
-                            .align(Alignment.CenterVertically)
-
+                        textStyle = TextStyle(color = Color(ColorPalette.pa50), fontSize = 16.sp),
                     )
-                }
-
-
-                Canvas(modifier = Modifier
-                    .fillMaxWidth().weight(0.5f))
-                {
-                    drawRect(
-                        brush = Brush.linearGradient(
-                            colors = listOf(
-                                Color(ColorPalette.sa10),
-                                Color(android.graphics.Color.parseColor("#00000000"))
-                            ),
-                            start = Offset(0f, 0f), // Top
-                            end = Offset(0f, size.height)
+                    if (searchQuery.isEmpty()) {
+                        Text(
+                            text = "Search Sets...",
+                            color = Color(ColorPalette.sa50), // Placeholder text color
+                            modifier = Modifier.align(Alignment.CenterStart)
                         )
-                    )
+                    }
                 }
+
             }
-            AnimatedVisibility(
-                visible = appear,
-                enter = fadeIn(animationSpec = tween(200)) + slideInHorizontally(
-                    animationSpec = tween(200)
-                ) { fullWidth -> -fullWidth / 2 },
-                exit = fadeOut(animationSpec = tween(200)) + slideOutHorizontally(
-                    animationSpec = tween(200)
-                ) { fullWidth -> -fullWidth / 2 }
-            ) {
+
+            Canvas(modifier = Modifier
+                .fillMaxWidth().weight(0.5f))
+            {
+                drawRect(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color(ColorPalette.sa10),
+                            Color(android.graphics.Color.parseColor("#00000000"))
+                        ),
+                        start = Offset(0f, 0f), // Top
+                        end = Offset(0f, size.height)
+                    )
+                )
+            }
+        }
+        AnimatedVisibility(
+            visible = appear,
+            enter = fadeIn(animationSpec = tween(200)) + slideInHorizontally(
+                animationSpec = tween(200)
+            ) { fullWidth -> -fullWidth / 2 },
+            exit = fadeOut(animationSpec = tween(200)) + slideOutHorizontally(
+                animationSpec = tween(200)
+            ) { fullWidth -> -fullWidth / 2 }
+        ) {
+            Row(modifier = Modifier
+                .background(Color(ColorPalette.sa10))
+                .fillMaxHeight()
+                .fillMaxWidth(0.4f)){
+
                 Column(modifier = Modifier
                     .background(Color(ColorPalette.sa10))
                     .fillMaxHeight()
-                    .fillMaxWidth(0.4f)){
+                    .weight(0.1f)){
                     SlideMenuContent()
                 }
+                Divider(color = Color(ColorPalette.sa30), modifier = Modifier.fillMaxHeight().width(2.dp))
             }
+
         }
     }
 }
-fun listFilesInAssets(context: Context) {
+fun  listFilesInAssets(context: Context) : Array<String>{
     try {
         val assetManager = context.assets
         val fileNames = assetManager.list("FlashcardDirectory")
         if (fileNames != null) {
-            // Log the names of all files
-            for (fileName in fileNames) {
-                Log.d("AssetsFiles", "File: $fileName")
-            }
+            return fileNames
         } else {
             Log.d("AssetsFiles", "No files found in the assets folder.")
         }
     } catch (e: Exception) {
         Log.e("AssetsFiles", "Error reading assets folder: ${e.message}")
     }
+    return arrayOf()
 }
 @Composable
 fun SlideMenuContent(){
@@ -263,4 +242,9 @@ fun SlideMenuContent(){
 
     }
 }
-
+@Preview(showBackground = true)
+@Composable
+fun preview(){
+    val context = LocalContext.current
+    MainMenuRender(context = context)
+}
